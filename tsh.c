113949fa6a97981ce36fb6ae98565b82be4a4875
kaustukrathod <kaustukrathod@outlook.com>
2017-03-05 13:49:03 +0530

    Update tsh.c

--- tsh.c
+++ tsh.c
@@ -1,7 +1,7 @@
 /* 
  * tsh - A tiny shell program with job control
  * 
- * <Put your name and login ID here>
+ * <Name : Kaustuk Rathod and id : 201501137  Email-Id : 201501137@daiict.ac.in>
  */
 #include <stdio.h>
 #include <stdlib.h>
